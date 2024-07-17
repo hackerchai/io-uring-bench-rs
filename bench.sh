c56d29d6f46d9f5ee2d2d258a5a7a94388cfc757
hackerchai <i@hackerchai.com>
2024-07-17 13:56:13 +0800

    feat(bench): modify bench params

--- bench.sh
+++ bench.sh
@@ -11,9 +11,9 @@ taskset -cp 0 $PID
 
 for bytes in 128 512 1000
 do
-	for connections in 1 50 150 300 500 1000
+	for connections in 50 150 300 500 1000
 	do
-   	/home/hackerchai/Developer/Rust/rust_echo_bench/target/release/echo_bench --address "localhost:$1" --number $connections --duration 10 --length $bytes
+   	/home/hackerchai/Developer/Rust/rust_echo_bench/target/release/echo_bench --address "localhost:$1" --number $connections --duration 60 --length $bytes
    	sleep 4
 	done
 done
